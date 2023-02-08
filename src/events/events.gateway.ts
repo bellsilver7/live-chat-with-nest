d554b803c2037940943a59673fa5e1cc5e9a1ae6
LeeJongEun <bellsilver7@jasongroup.co.kr>
2023-02-08 11:36:28 +0900

    feat: 이벤트 게이트웨이 테스트 적용

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,17 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(): void {
+    this.server.emit('message', 'Hello World!');
+  }
+}
